a8b3e9f7853eac3e7b049e521b86772ac5b3c998
crisjohn.dev.official@gmail.com <crisjohn.dev.official@gmail.com>
2026-09-19 07:24:38 +0800

    hjfjhb

--- config/settings.py
+++ config/settings.py
@@ -149,8 +149,4 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
 }
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
-
+CORS_ALLOW_ALL_ORIGINS = True
